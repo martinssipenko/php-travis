2d8c7e221a43ef4cd331ef644d5fe139e01593fb
Martins Sipenko <martins.sipenko@accenture.com>
2013-06-09 18:09:08 +0000

    testing travis-ci

--- build/before_script.sh
+++ build/before_script.sh
@@ -11,6 +11,6 @@ sudo service nginx start
 sleep 3
 
 vendor/bin/sauce_config $SAUCE_USERNAME $SAUCE_ACCESS_KEY
-vendor/bin/sauce_connect > /dev/null 2>&1 &
+vendor/bin/sauce_connect
 
 printf "\n=== The CI environment has been initialized ===\n"
