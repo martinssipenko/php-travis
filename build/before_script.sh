03a855a7ea808064fcd4212dd2e5aeb0b6311c93
Martins Sipenko <martins.sipenko@accenture.com>
2013-06-09 15:44:11 +0000

    updated

--- build/before_script.sh
+++ build/before_script.sh
@@ -5,6 +5,8 @@ echo "=== Initializing CI environment ==="
 composer self-update
 composer install --dev
 
+vendor/bin/sauce_config $SAUCE_USERNAME $SAUCE_ACCESS_KEY
+
 chmod +x ./build/script.sh
 
 printf "\n=== The CI environment has been initialized ===\n"
--- build/script.sh
+++ build/script.sh
@@ -5,4 +5,5 @@ echo "=== Starting tests ==="
 php -l lib/
 vendor/bin/phploc --count-tests --exclude vendor/ lib/ tests/ > build/logs/phploc.txt
 vendor/bin/phploc --count-tests --log-xml build/logs/phploc.xml lib/ tests/
-vendor/bin/phpunit
+vendor/bin/paratest --phpunit=vendor/bin/phpunit
+vendor/bin/paratest --phpunit=vendor/bin/phpunit vendor/sauce/sausage/WebDriverDemo.php
