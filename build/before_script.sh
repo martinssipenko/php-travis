8c7cfac5fba364a9dd54f4399be38a3ab605367d
Martins Sipenko <martins.sipenko@accenture.com>
2013-06-10 17:54:56 +0000

    .

--- build/before_script.sh
+++ build/before_script.sh
@@ -24,4 +24,6 @@ mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 wget -nv -O /tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
 sudo tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WWW_ROOT
 
+ls -la $WWW_ROOT
+
 printf "\n=== The CI environment has been initialized ===\n"
