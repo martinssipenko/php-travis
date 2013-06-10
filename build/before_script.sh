b7815c12ac39ce1cf85fda42784fe08c94ae4fa1
Martins Sipenko <martins.sipenko@accenture.com>
2013-06-10 18:14:50 +0000

    nginxx

--- build/before_script.sh
+++ build/before_script.sh
@@ -17,9 +17,12 @@ sudo cp ./build/travis_nginx.conf /etc/nginx/nginx.conf
 sudo service nginx start
 sleep 3
 
+mysql -e 'CREATE DATABASE wordpress_test;' -uroot
+
 export WWW_ROOT=/usr/share/nginx/www
 sudo rm -r $WWW_ROOT
-mysql -e 'CREATE DATABASE wordpress_test;' -uroot
+sudo mkdir -p $WWW_ROOT
+sudo chown www-data $WWW_ROOT
 
 wget -nv -O /tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
 sudo tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WWW_ROOT
