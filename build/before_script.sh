f9e7baeeb8faf2fc4371aaf06ddf1a7116325902
Martins Sipenko <martins.sipenko@accenture.com>
2013-06-10 17:26:49 +0000

    install php & wp

--- build/before_script.sh
+++ build/before_script.sh
@@ -10,9 +10,15 @@ chmod +x ./build/script.sh
 composer self-update
 composer install --prefer-source --no-interaction --dev
 
+cp ./build/travis_nginx.conf /etc/nginx/nginx.conf
+
 sudo service nginx start
 sleep 3
 
-vendor/bin/sauce_config $SAUCE_USERNAME $SAUCE_ACCESS_KEY
+export WWW_ROOT=/usr/share/nginx/www
+mysql -e 'CREATE DATABASE wordpress_test;' -uroot
+
+wget -nv -O /tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
+tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WWW_ROOT
 
 printf "\n=== The CI environment has been initialized ===\n"
